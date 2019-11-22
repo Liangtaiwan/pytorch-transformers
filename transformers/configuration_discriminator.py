58283c12de3c597db4a33e62ccdf83eef4fe2363
Chi-Liang <liangtaiwan1230@gmail.com>
2019-11-22 20:02:37 +0800

    update dis config,model

---
+++ transformers/configuration_discriminator.py
@@ -0,0 +1,27 @@
+""" Discriminator model configuration """
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import json
+import logging
+import sys
+from io import open
+
+from .configuration_utils import PretrainedConfig
+
+logger = logging.getLogger(__name__)
+
+
+class DiscriminatorConfig(PretrainedConfig):
+    pretrained_config_archive_map = BERT_PRETRAINED_CONFIG_ARCHIVE_MAP
+    def __init__(self,
+                 config_json_file=None,
+                 **kwargs):
+        super(BertConfig, self).__init__(**kwargs)
+        if isinstance(config_json_file, str):
+            with open(config_json_file, "r", encoding='utf-8') as reader:
+                json_config = json.loads(reader.read())
+            for key, value in json_config.items():
+                self.__dict__[key] = value
+        else:
+            raise ValueError("First argument must be the path to a discriminators config file (str)")
